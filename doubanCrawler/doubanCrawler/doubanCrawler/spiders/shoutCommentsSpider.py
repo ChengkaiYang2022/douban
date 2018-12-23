4378b6a38044ede846c6e46106b4d32fd7a36175
forrestlmj <18810276930@163.com>
2018-12-23 22:38:50 +0800

    豆瓣拿页面初步

--- doubanCrawler/doubanCrawler/doubanCrawler/spiders/shoutCommentsSpider.py
+++ doubanCrawler/doubanCrawler/doubanCrawler/spiders/shoutCommentsSpider.py
@@ -3,15 +3,10 @@ class shoutCommentsSpider(scrapy.Spider):
     name = "shoutComment"
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://movie.douban.com/subject_search?search_text=闺密&cat=1002'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     def parse(self,response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        page = response.text()
 
