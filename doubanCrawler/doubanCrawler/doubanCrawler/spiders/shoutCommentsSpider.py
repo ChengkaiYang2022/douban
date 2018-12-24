b096a194c48e486eb98a5fa233b9f7a4aaaefd0b
forrestlmj <18810276930@163.com>
2018-12-24 10:01:38 +0800

    页面访问改为https://www.douban.com/search?cat=1002&q=闺蜜
    Signed-off-by: forrestlmj <18810276930@163.com>

--- doubanCrawler/doubanCrawler/doubanCrawler/spiders/shoutCommentsSpider.py
+++ doubanCrawler/doubanCrawler/doubanCrawler/spiders/shoutCommentsSpider.py
@@ -3,10 +3,11 @@ class shoutCommentsSpider(scrapy.Spider):
     name = "shoutComment"
     def start_requests(self):
         urls = [
-            'https://movie.douban.com/subject_search?search_text=闺密&cat=1002'
+            # 'https://movie.douban.com/subject_search?search_text=闺密&cat=1002'
+            'https://www.douban.com/search?cat=1002&q=闺蜜'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse,dont_filter=True)
     def parse(self,response):
         page = response.text()
 
