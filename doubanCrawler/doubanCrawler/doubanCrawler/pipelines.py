2cb5bac198d896664680a587c4062309b9f8ae59
forrestlmj <18810276930@163.com>
2019-01-22 20:48:24 +0800

    增加时间戳

--- doubanCrawler/doubanCrawler/doubanCrawler/pipelines.py
+++ doubanCrawler/doubanCrawler/doubanCrawler/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class DoubancrawlerPipeline(object):
+class DoubancrawlerMongoPipeline(object):
     def process_item(self, item, spider):
         return item
